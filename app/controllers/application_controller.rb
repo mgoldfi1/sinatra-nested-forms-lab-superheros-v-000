963135d9223adcc6c2c712254b1a0fdafb3ed42f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-03 14:49:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
+    get '/' do
+      erb :index
+    end
+    post '/teams' do
+      @team_name = params["team"]["name"]
+      @team_motto = params["team"]["motto"]
+      @h1n = params["team"]["members"][0]["name"]
+      @h1p = params["team"]["members"][0]["power"]
+      @h1b = params["team"]["members"][0]["bio"]
+      @h2n = params["team"]["members"][1]["name"]
+      @h2p = params["team"]["members"][1]["power"]
+      @h2b = params["team"]["members"][1]["bio"]
+      @h3n = params["team"]["members"][2]["name"]
+      @h3p = params["team"]["members"][2]["power"]
+      @h3b = params["team"]["members"][2]["bio"]
+      erb :team
+    end
 end
