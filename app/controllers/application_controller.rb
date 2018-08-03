318e4b48aeb73a8ef802214dd3cba1facea9dec1
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-03 14:37:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,12 @@ class App < Sinatra::Base
       @h1n = params["member1_name"]
       @h1p = params["member1_power"]
       @h1b = params["member1_bio"]
+      @h2n = params["member2_name"]
+      @h2p = params["member2_power"]
+      @h2b = params["member2_bio"]
+      @h3n = params["member1_name"]
+      @h3p = params["member1_power"]
+      @h3b = params["member1_bio"]
       erb :team
     end
 end
